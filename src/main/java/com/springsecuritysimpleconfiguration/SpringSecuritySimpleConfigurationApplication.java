3f66e35907036e01fe2a32e5c42dbe9addb64523
zhur4k <kostya.zhurovich12052@gmail.com>
2024-12-26 22:00:03 +0300

    feat:init project.

---
+++ src/main/java/com/springsecuritysimpleconfiguration/SpringSecuritySimpleConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.springsecuritysimpleconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySimpleConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecuritySimpleConfigurationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springsecuritysimpleconfiguration/SpringSecuritySimpleConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecuritysimpleconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySimpleConfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
